93f7c4aed1188d05a398dc9bb2cecd0b4df0f4d7
johannpereze <sebas.jsp@gmail.com>
2022-01-16 10:37:48 -0500

    feat: finished basic structure of the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import avatar from './assets/avatar.jpg'
 import './App.css';
 
-function App() {
+export const App = () => {
+
+  const initialState = {
+    storyTitle: "Story Title",
+    storySubtitle: "Episode 1",
+    userLifePoints: 4,
+    userAvatarUrl: avatar,
+    soundControl: true,
+    instructionsTitle: "Let's practice",
+    instructionsBody: "Put the words in order to make a correct sentence.",
+  }
+
+  const [state] = useState(initialState)
+
+  const { storyTitle,
+    storySubtitle,
+    userLifePoints,
+    userAvatarUrl,
+    soundControl,
+    instructionsTitle,
+    instructionsBody } = state
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <div className="header__logo">{"logo"}</div>
+        <div className="header__story-title">{storyTitle}</div>
+        <div className="header__story-subtitle">{storySubtitle}</div>
+        <div className="header__life-points">{userLifePoints}</div>
+        <div className="header__user-avatar"><img src={userAvatarUrl} alt="user" /></div>
+        <div className="header__sound-control">{JSON.stringify(soundControl)}</div>
       </header>
-    </div>
-  );
+      <main className="exercise">
+        <div className="exercise__instructions--container">
+          <h2>{instructionsTitle}</h2>
+          <p>{instructionsBody}</p>
+        </div>
+        <div className="exercise__options--container">
+          <div className="exercise__options--reset-control"><p>reset</p></div>
+          <div className="exercise__options--input">
+            <input type="text" name="" id="" />
+          </div>
+          <div className="exercise__options--answers">
+            <button>answer</button>
+          </div>
+        </div>
+      </main>
+    </>
+  )
 }
 
-export default App;
